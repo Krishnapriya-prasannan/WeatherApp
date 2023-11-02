2905031b873825c8efde29421e94466371e438e3
Krishnapriya Prasannan <krishnapriyaprasannan1@gmail.com>
2023-11-02 20:17:06 +0530

    code with run time error

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '238f3a4b08msh4d78ffb836e6730p1ac92ajsn306c8f526cd4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo ";
+    
