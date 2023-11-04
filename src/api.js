bd6ccbdbe5fb84c3a0e973a597c9f9b0c01df68f
ColinJimmy <111682579+ColinJimmy@users.noreply.github.com>
2023-11-04 14:23:50 +0530

    new commit

--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
 
+// export const geoApiOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '238f3a4b08msh4d78ffb836e6730p1ac92ajsn306c8f526cd4',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+// export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo ";
+    
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '238f3a4b08msh4d78ffb836e6730p1ac92ajsn306c8f526cd4',
+		'X-RapidAPI-Key': '6ffa4bbe36mshe970d7ac0a64f4bp1fe3d5jsnab27a64e271d',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
-export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo ";
-    
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
